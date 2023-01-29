82ca2f651c9e5721321ee710f686531dc81ebba4
Tekin Ozkilic <mtekinozk@gmail.com>
2023-01-29 19:35:17 +0300

    bootstrap bölüm sonu çalışması djangoya eklendi

--- page/views.py
+++ page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+context = {}
 def home(request):
-    return HttpResponse("Ana Sayfaya Hoşgeldiniz")
+    return render(request,"page/index.html",context)
